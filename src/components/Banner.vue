477b24624b0db289a715227f0636cd6950db067a
mclaramarinho <marinho.claramb@gmail.com>
2024-01-03 18:33:28 -0300

    Add Banner component with responsive styling

---
+++ src/components/Banner.vue
@@ -0,0 +1,84 @@
+<template>
+    <header class="banner">
+      <div class="apresentacao">
+        <img src="../assets/imagens-main/images/logo.svg" alt="" class="logo">
+        <p class="cabecalho-lg frase-cabecalho">
+          <span class="texto-verde">Um banquete de ideias para</span>
+          despertar o chef que há em você!
+        </p>
+        <p class="subtitulo-lg">
+          Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+        </p>
+      </div>
+      <img src="../assets/imagens-main/images/foto-banner.png" alt="" class="foto-banner">
+    </header>
+  </template>
+
+
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.frase-cabecalho {
+  margin-bottom: 2rem;
+}
+
+.texto-verde {
+  color: var(--verde-medio, #3D6D4A);
+}
+
+.foto-banner {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .foto-banner {
+    width: min(100%, 21.25rem);
+  }
+
+  .cabecalho-lg {
+    font-size: 2.75em;
+  }
+
+  .cabecalho {
+    font-size: 2.3125em;
+  }
+
+  .subtitulo-lg {
+    font-size: 1.625em;
+  }
+}
+
+
+</style>
