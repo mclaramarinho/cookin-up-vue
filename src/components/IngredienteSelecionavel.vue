496918aef951fc110237188aae52899fb635c8c6
mclaramarinho <marinho.claramb@gmail.com>
2024-01-03 18:33:47 -0300

    Add IngredienteSelecionavel component

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+
+export default{ 
+    components: { Tag },
+    props: {
+        ingrediente: {type: String, required: true},
+        isSelecionado: {type: Boolean}
+    },
+    data(){
+        return {
+            selecionado: this.isSelecionado
+        }
+    },
+    methods: {
+        handleClick():void{
+            this.selecionado = !this.selecionado;
+            if(this.selecionado){
+                this.$emit("adicionarIngrediente", this.ingrediente)
+            }else{
+                this.$emit("removerIngrediente", this.ingrediente)
+            }
+        }
+    },
+    emits: ["adicionarIngrediente", "removerIngrediente"]
+}
+</script>
+
+<template>
+    <button class="ingrediente" @mouseup="handleClick" :aria-pressed="selecionado">
+        <Tag :texto="ingrediente" :ativa="selecionado" />
+    </button>
+</template>
+
+<style scoped>
+.ingrediente{
+    cursor: pointer;
+}
+</style>
