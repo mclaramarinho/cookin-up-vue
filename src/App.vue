d8166366b082188621f8e243c58498970650b2d0
mclaramarinho <marinho.claramb@gmail.com>
2024-01-03 18:32:25 -0300

    Add App.vue component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import Conteudo from './components/Conteudo.vue';
+import RodapeVue from './components/Rodape.vue';
+export default {
+  components: { Banner, Conteudo, RodapeVue }
+}
+</script>
+
+<template>
+  <Banner />
+  <Conteudo />
+  <RodapeVue />
+</template>
+
